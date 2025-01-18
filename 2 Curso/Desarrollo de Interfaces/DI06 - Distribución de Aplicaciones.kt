f734a0d458b2e4b8488b20dc4ca5ea08a562d271
juarezracing <101058689+juarezracing@users.noreply.github.com>
2025-01-18 12:20:33 +0100

    Create DI06 - Distribución de Aplicaciones.kt

---
+++ 2 Curso/Desarrollo de Interfaces/DI06 - Distribución de Aplicaciones.kt
@@ -0,0 +1,5 @@
+### DISTRIBUCIÓN DE APLICACIONES
+### DEFINICIÓN Y COMPOSICIÓN DE UNA DISTRIBUCIÓN
+Una distribución de software es un conjunto de programas específicos que se presenta compilado y configurado.
+
+Licencia **GPL** General Public License, creada para proteger la libre distribución
